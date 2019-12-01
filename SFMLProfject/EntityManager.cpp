6186e9ac1138692dd9745116947a24df9fd7f4aa
xxzoltanxx <antonio.franjcic1@gmail.com>
2019-12-01 11:09:40 +0100

    Flag fix, System manager used in entity manager

--- SFMLProfject/EntityManager.cpp
+++ SFMLProfject/EntityManager.cpp
@@ -1,18 +1,21 @@
 #include "EntityManager.h"
+#include "SystemManager.h"
 void EntityManager::addComponent(int entityId, ComponentType type)
 {
 	auto& entity = mEntities[entityId];
 	entity.flag |= (1 << (int)type);
 	entity.components.push_back(componentFactory[type]());
+	systemManager->entityModified(entityId, entity.flag);
 }
 
 void EntityManager::removeComponent(int entityId, ComponentType type)
 {
 	auto& entity = mEntities[entityId];
-	entity.flag |= ~(1 << (int)type);
+	entity.flag &= ~(1 << (int)type);
 	auto Component = std::find_if(entity.components.begin(), entity.components.end(), [type](CBase* item)-> bool {return item->getType() == type; });
 	delete (*Component);
 	entity.components.erase(Component);
+	systemManager->entityModified(entityId, entity.flag);
 }
 
 int EntityManager::addEntity(int flag)
@@ -26,6 +29,7 @@ int EntityManager::addEntity(int flag)
 			addComponent(id, ComponentType(i));
 		}
 	}
+	systemManager->entityModified(id, flag);
 	return id;
 }
 
@@ -36,5 +40,6 @@ void EntityManager::removeEntity(int id)
 	{
 		delete iter->second.components[i];
 	}
+	systemManager->removeEntity(id);
 	mEntities.erase(iter);
 }
